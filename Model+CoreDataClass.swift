95e8e67005eefd180871966f40ea4a377069a8bf
elizavetaosipova <evosipova_3@edu.hse.ru>
2023-07-14 21:22:00 +0300

    CoreData

---
+++ Model+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Model+CoreDataClass.swift
+//  ToDo
+//
+//  Created by Elizaveta Osipova on 7/14/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Model)
+public class Model: NSManagedObject {
+
+}
